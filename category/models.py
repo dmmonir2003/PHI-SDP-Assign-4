89741acfe2d01a8343c93d754d67ea3bbf74cfbd
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-14 23:56:10 +0600

    create task model and category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from task.models import TaskModel
 # Create your models here.
+
+class CategoryModel(models.Model):
+    name=models.CharField(max_length=100)
+    category=models.ManyToManyField(TaskModel)
--- task/models.py
+++ task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TaskModel(models.Model):
+    task_title=models.CharField(max_length=100)
+    task_description=models.TextField()
+    is_completed=models.BooleanField(default=False)
+    task_assign_date=models.DateField()
